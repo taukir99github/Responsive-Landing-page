5cb10c8f396e5c815a405d36e3d0f4388d3fc4d9
Md Taukir <70102701+taukir99github@users.noreply.github.com>
2023-08-13 12:38:13 +0530

    hii

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import {AiOutlineMenuFold} from 'react-icons/ai';
+
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <h2>Logo</h2>
+        <div className="nav-list">
+          <h4>Home</h4>
+          <h4>Services</h4>
+          <h4>About</h4>
+          <h4>Support</h4>
+          <button>Sign In</button>
+          <p><AiOutlineMenuFold/></p>
+        </div>
+      </nav>
+      <div className="content">
+      <div className="content-left">
+       <h2> Hello Learn <span>Coding</span> from me</h2>
+       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo ratione doloribus odit fugit vero consectetur tempora laudantium omnis? Ab, consectetur.</p>
+       <button>Explore Now</button>
+      </div>
+      <div className="content-right">
+        <img src="./pic.jpg" alt="" />
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
